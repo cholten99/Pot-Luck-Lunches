9feb221bf1a045c60237b57733650da725dea585
David Durant <dave@bowsy.co.uk>
2014-07-23 02:09:27 +0100

    Push to prod (remember to change config.inc.php)

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -28,7 +28,8 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'plldb';
+$cfg['Servers'][$i]['socket'] = 'tunnel.pagodabox.com';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
